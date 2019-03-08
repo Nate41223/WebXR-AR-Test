460b1f6d6bf1f1928a24cb2f2848ede9ad32aed9
Nathan Stenman <stenman@ferris.edu>
2019-03-08 16:01:05 -0500

    testing

---
+++ main.js
@@ -0,0 +1,24 @@
+AFRAME.registerComponent("box", {
+    schema: {
+        width: {type: 'number', default: 1},
+        height: {type: 'number', default: 1},
+        depth: {type: 'number', default: 1},
+        position: {type: 'vec3', default: {x:1, y:1, z:1}},
+        color: {type: 'color', default: '#AAA'}
+    },
+
+    init: function() {
+        var data = this.data;
+        var el = this.el;
+
+        this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data,depth);
+
+        this.material = new THREE.MeshStandardMaterial({color: data.color});
+
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+
+        this.position = data.position;
+
+        el.setObject3D('mesh', this.mesh);
+    }
+});
