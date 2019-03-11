7381a5dc6adbbe70857f9d0a77d65c8de0962ae0
Nathan Stenman <stenman@ferris.edu>
2019-03-11 14:29:11 -0400

    test

--- main.js
+++ main.js
@@ -4,20 +4,20 @@ AFRAME.registerComponent('box', {
         height: {type: 'number', default: .2},
         depth: {type: 'number', default: .2},
         position: {type: 'vec3', default: {x:0, y:0, z:-2}},
-        color: {type: 'color', default: '#f00'}
+        color: {type: 'color', default: 'red'}
     },
 
     init: function() {
         var data = this.data;
         var el = this.el;
 
-        this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data,depth);
+        this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
 
         this.material = new THREE.MeshStandardMaterial({color: data.color});
 
         this.mesh = new THREE.Mesh(this.geometry, this.material);
 
-        //this.position = data.position;
+        this.position = data.position;
 
         el.setObject3D('mesh', this.mesh);
     }
