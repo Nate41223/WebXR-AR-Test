bc6d8eb4a3d40d0139ede0bb66557680218a6778
Nathan Stenman <stenman@ferris.edu>
2019-03-19 16:54:32 -0400

    tests

--- primitives.js
+++ primitives.js
@@ -2,11 +2,9 @@ AFRAME.registerPrimitive('a-boxtest', {
     defaultComponents: {
         geometry: {primitive: 'box'},
         material: {color: 'red'},
-        position: {},
         scale: {x:.25, y:.25, z:.25},
     },
 
     mappings: {
-        position: 'position',
     },
 });
