aff72611be235236279b858947a1a184f1e04b65
Nathan Stenman <stenman@ferris.edu>
2019-03-19 14:02:17 -0400

    test

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ AFRAME.registerComponent('box', {
         this.cE = function() {
             el.setAttribute('color', data.colorChange);
             fishFoodCount++;
-            document.getElementById("fish") = document.write(fishFoodCount);
+            document.getElementById("fish").innerHTML = document.write(fishFoodCount);
         };
         this.setupEvents();
     },
